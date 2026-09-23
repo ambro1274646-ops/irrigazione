bbcf541eb2024a58665469a1d30b1d6635827ef3
ambro1274646-ops <ambro1274646@gmail.com>
2026-09-23 16:01:19 +0200

    Add device handling and update configuration file

--- CoordinatoreIrrigazione/CoordinatoreIrrigazione/Program.cs
+++ CoordinatoreIrrigazione/CoordinatoreIrrigazione/Program.cs
@@ -20,6 +20,7 @@ namespace programma_aggiornamento_raspberry
             public string ora;
             public string minuto;
             public double durata;
+            public string dispositivi;
         }
 
         public class linea_da_inserire
@@ -30,12 +31,13 @@ namespace programma_aggiornamento_raspberry
         static void Main(string[] args)
         {
             //Leggo url_config.ini ed estraggo l'url:
-            string url = "http://127.0.0.1/schedulazione.php?id_utente=1&id_impianto=1";
+            string url = "http://127.0.0.1/schedulazione.php?id_utente=1&id_impianto=1&i=1";
             //string url = File.ReadAllText("/irrigazione/FILE_CONFIG/url_config.ini").Trim('\n', ' ');
             JObject jobj = new JObject();
 
             using (HttpClient client = new HttpClient())
             {
+               
                 var risposta = client.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
                 jobj = JObject.Parse(risposta);
             }
@@ -46,30 +48,33 @@ namespace programma_aggiornamento_raspberry
             {
                 //Creo un oggetto schedulazione giorno chiamato gg per ogni giorno della settimana:
                 schedulazione_giorno gg = new schedulazione_giorno();
-                gg.giorno = Convert.ToInt32(jobj["schedulazione_settimana"][i]["nome"]);
+                gg.giorno = Convert.ToInt32(jobj["schedulazione_settimana"][i]["giorno"]);
                 //Controllo se non è null, se null allora inserisco in ora e in minuto null come stringa, altrimenti inserisco i valori dallo split
-                if(jobj["schedulazione_settimana"][i]["orario"].ToString() == "null")
+                if (jobj["schedulazione_settimana"][i]["orario"].ToString() == "null")
                 {
                     gg.minuto = "null";
                     gg.ora = "null";
                     gg.durata = 0;
+                    gg.dispositivi = "null";
                 }
                 else
                 {
                     gg.ora = jobj["schedulazione_settimana"][i]["orario"].ToString().Split(':')[0];
                     gg.minuto = jobj["schedulazione_settimana"][i]["orario"].ToString().Split(':')[1];
-                    gg.durata= Convert.ToDouble(jobj["schedulazione_settimana"][i]["durata"]);
+                    gg.durata = Convert.ToDouble(jobj["schedulazione_settimana"][i]["durata"]);
+                    gg.dispositivi = jobj["schedulazione_settimana"][i]["dispositivi"].ToString();
                 }
                 schedulazione_settimana.Add(gg);
             }
 
             //Inserisco le durate nel file di configurazione del programma attuatore:
-            
+
 
 
             //------------Aggiorno crontab------------//
             //Elimino Righe che contengono il nome del programma con sed:
-            /*
+
+            
             String parameters = "-i /apri_pompa.exe/d /var/spool/cron/crontabs/root";
             ProcessStartInfo cmdsi = new ProcessStartInfo(@"sed");
             cmdsi.Arguments = parameters;
@@ -100,8 +105,21 @@ namespace programma_aggiornamento_raspberry
             {
                 File.AppendAllText("/var/spool/cron/crontabs/root", lin.linea + "\n");
             }
+            
 
-            */
+            //------Aggiorno File Config per comunicazione arduino-------//
+            //irrigazione/FILE_CONFIG/durata_disp.ini
+            List<string> contenuto_file = new List<string>();
+            foreach(schedulazione_giorno giorn in schedulazione_settimana)
+            {
+                if (giorn.dispositivi != "null")
+                {
+                    //Struttura: giorn%disp|disp|...%durata
+                    contenuto_file.Add(Convert.ToString(giorn.giorno) + "%" + giorn.dispositivi + "%" + Convert.ToString(giorn.durata));
+                }
+            }
+            File.WriteAllLines("irrigazione/FILE_CONFIG/durata_disp.ini", contenuto_file);
+
         }
     }
 }
