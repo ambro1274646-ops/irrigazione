f4e6caf4b46549d0792d98be5971030c3fd4ac87
ambro1274646-ops <ambro1274646@gmail.com>
2026-09-23 16:23:06 +0200

    Change database connection to local host
    Updated database connection parameters to local settings.

--- sito_modifiche_live/dashboard.php
+++ sito_modifiche_live/dashboard.php
@@ -1,9 +1,9 @@
 <?php
     // dashboard.php
 
-$host     = "sql300.infinityfree.com";
-$username = "if0_42967232";
-$password = "Ambro666Ambro";
+$host     = "127.0.0.1";
+$username = "root";
+$password = "";
 $database = "if0_42967232_irrigazione_db";
 
 // 1) Prendi il token dal cookie
